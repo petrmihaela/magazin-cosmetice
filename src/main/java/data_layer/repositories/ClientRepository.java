fa8e5be7ba7aff5282d208ec16cc2ac0a7ce6aa7
MihaelaPetrisor <petr.mihaela97@gmail.com>
2019-01-06 14:39:39 +0200

    uniq username

--- src/main/java/data_layer/repositories/ClientRepository.java
+++ src/main/java/data_layer/repositories/ClientRepository.java
@@ -4,8 +4,11 @@ import data_layer.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Customer, Integer> {
 
     Customer findByUsernameAndParola(String username, String parola);
+
 }
--- src/main/java/data_layer/services/ClientService.java
+++ src/main/java/data_layer/services/ClientService.java
@@ -8,6 +8,8 @@ import data_layer.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClientService {
 
@@ -15,17 +17,22 @@ public class ClientService {
     ClientRepository clientRepository;
 
     public int saveClient(Customer c) {
+        List<Customer> customers = clientRepository.findAll();
+        for (Customer added : customers) {
+            if (added.getUsername().equals(c.getUsername()))
+                return -1;
+        }
         Customer added = clientRepository.saveAndFlush(c);
         return added.getId();
     }
 
     public LoggedClient getClient(ClientDto clientDto) throws NotFoundException {
         Customer customer = clientRepository.findByUsernameAndParola(clientDto.getUsername(), clientDto.getParola());
-        if(customer == null){
+        if (customer == null) {
             throw new NotFoundException("Username or password is wrong!");
         }
 
-        LoggedClient loggedClient=new LoggedClient(customer.getId(),customer.getNume(),customer.getPrenume(),customer.getTelefon(),customer.getAdresa());
+        LoggedClient loggedClient = new LoggedClient(customer.getId(), customer.getNume(), customer.getPrenume(), customer.getTelefon(), customer.getAdresa());
         return loggedClient;
     }
 }
